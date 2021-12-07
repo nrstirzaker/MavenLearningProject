2c46b2c4a18bb0b778a956fad7680b2094f2d36d
Nigel <nigel.stirzaker@nielsen.com>
2021-12-07 14:36:58 +0000

    Basic Maven Project Setup

---
+++ src/main/java/com/nielseniq/book/nigel/learning/MainClass.java
@@ -0,0 +1,14 @@
+package com.nielseniq.book.nigel.learning;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainClass {
+	
+	private static Logger LOG = LoggerFactory.getLogger(MainClass.class);
+	
+	public static void main(String[] args) {
+		LOG.info("Has Run");
+	}
+
+}
